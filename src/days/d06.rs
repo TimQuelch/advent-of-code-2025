d46f7bf9830451192cda352e61f7b4be50696fd7
Tim Quelch <tim@tquelch.com>
2026-01-03 20:39:36 +1100

    Day 6 part 2

--- src/days/d06.rs
+++ src/days/d06.rs
@@ -1,8 +1,9 @@
 use itertools::Itertools;
-use ndarray::Array2;
-use std::iter::zip;
+use ndarray::{Array2, Array3};
+use regex::Regex;
+use std::iter::{self, zip};
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Op {
     Add,
     Mul,
@@ -13,6 +14,16 @@ impl From<&str> for Op {
         match x {
             "+" => Op::Add,
             "*" => Op::Mul,
+            _ => panic!("invalid op {x}"),
+        }
+    }
+}
+
+impl From<u8> for Op {
+    fn from(x: u8) -> Self {
+        match x {
+            b'+' => Op::Add,
+            b'*' => Op::Mul,
             _ => panic!("invalid op {x}"),
         }
     }
@@ -55,8 +66,73 @@ pub fn part1(input: &str) -> i64 {
         .unwrap()
 }
 
-pub fn part2(_input: &str) -> i64 {
-    0
+pub fn part2(input: &str) -> i64 {
+    let ops_str = input.trim_end_matches('\n').rsplit_once('\n').unwrap().1;
+    let ops_bytes = ops_str.as_bytes();
+    let ops: Vec<(Op, u8)> = Regex::new(r"[*+]\s+")
+        .unwrap()
+        .find_iter(ops_str)
+        .map(|m| {
+            // Need to subtract one off all values except the last one to account for the separator
+            // between columns
+            let len = if m.end() == ops_str.len() {
+                m.len()
+            } else {
+                m.len() - 1
+            };
+            (ops_bytes[m.start()].into(), len.try_into().unwrap())
+        })
+        .collect();
+
+    // Do some array indexing black magic to get the values in the correct order. Essentially we are
+    // swapping around the order of the axes from (row, column, digit) to (column, row, digit). This
+    // means that the digits of each number are contiguous
+    let vals = {
+        let ncols = ops.len();
+        let nrows = input.trim().lines().count() - 1;
+        let max_width = ops.iter().max_by_key(|&&(_, w)| w).unwrap().1 as usize;
+
+        // Construct the vector in same order as input string
+        let mut raw = Vec::with_capacity(ncols * nrows * max_width);
+        for line in input.lines().take(nrows).map(str::as_bytes) {
+            let mut offset: usize = 0;
+            for w in ops.iter().map(|&(_, w)| w as usize) {
+                raw.extend_from_slice(&line[offset..(offset + w)]);
+                raw.extend(iter::repeat_n(b' ', max_width - w)); // Pad columns to the same width
+                offset += w + 1; // Additional increment to skip space separator
+            }
+        }
+
+        // Create array from raw vec in same order
+        let raw_array = Array3::from_shape_vec((nrows, ncols, max_width), raw).unwrap();
+
+        // Permute axes into desired order and create final array
+        Array3::from_shape_vec(
+            (ncols, max_width, nrows),
+            raw_array.permuted_axes([1, 2, 0]).iter().copied().collect(),
+        )
+        .unwrap()
+    };
+
+    zip(vals.outer_iter(), ops.iter().map(|&(op, _)| op))
+        .map(|(ns, op)| -> u64 {
+            // Filter out values which error on parsing. These values are digit-columns full of
+            // spaces where the columns have been padded out
+            let n_iter = ns.outer_iter().filter_map(|n| {
+                str::from_utf8(n.as_slice().unwrap())
+                    .unwrap()
+                    .trim()
+                    .parse::<u64>()
+                    .ok()
+            });
+            match op {
+                Op::Add => n_iter.sum(),
+                Op::Mul => n_iter.product(),
+            }
+        })
+        .sum::<u64>()
+        .try_into()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -72,13 +148,13 @@ mod tests {
 
     #[test]
     fn example_part1() {
-        let result = part1(EXAMPLE.trim());
+        let result = part1(EXAMPLE.trim_start());
         assert_eq!(result, 4277556);
     }
 
     #[test]
     fn example_part2() {
-        let result = part2(EXAMPLE.trim());
-        assert_eq!(result, 0);
+        let result = part2(EXAMPLE.trim_start());
+        assert_eq!(result, 3263827);
     }
 }
