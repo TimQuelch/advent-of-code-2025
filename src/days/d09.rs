136201f4a6105c0b420847ce6ff2af82283ff6a2
Tim Quelch <tim@tquelch.com>
2026-01-04 18:26:25 +1100

    WIP Day 9 part 2

--- src/days/d09.rs
+++ src/days/d09.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use itertools::Itertools;
 
 fn parse_positions(input: &str) -> Vec<(u64, u64)> {
@@ -23,8 +25,28 @@ pub fn part1(input: &str) -> i64 {
         .unwrap()
 }
 
-pub fn part2(_input: &str) -> i64 {
-    0
+pub fn part2(input: &str) -> i64 {
+    let positions = parse_positions(input);
+    let positions_set: HashSet<_> = positions.iter().collect();
+
+    // let vertical_edges: BTreeMap<_> = positions.iter().
+
+    let point_in_polygon = |x: (u64, u64)| -> bool {
+        if positions_set.contains(&x) {
+            return true;
+        }
+
+        true
+    };
+
+    positions
+        .iter()
+        .cartesian_product(positions.iter())
+        .filter_map(|(&(x1, y1), &(x2, y2))| Some((x1.abs_diff(x2) + 1) * (y1.abs_diff(y2) + 1)))
+        .max()
+        .unwrap()
+        .try_into()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -51,6 +73,6 @@ mod tests {
     #[test]
     fn example_part2() {
         let result = part2(EXAMPLE.trim());
-        assert_eq!(result, 0);
+        assert_eq!(result, 24);
     }
 }
